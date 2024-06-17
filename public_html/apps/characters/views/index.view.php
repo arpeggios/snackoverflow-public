2b2673c4c5160788d4bff0476b5b89d73a497607
JM <jvcmoore@gmail.com>
2024-06-16 22:32:11 -0400

    minor html and css cleanup

--- public_html/apps/characters/views/index.view.php
+++ public_html/apps/characters/views/index.view.php
@@ -11,15 +11,13 @@
       <?php foreach ($characters as $character) : ?>
         <tr>
           <td>
-            <a href='character.php?<?php echo http_build_query(['mode' => 'view', 'character' => $character->charName, 'id' => $character->id]); ?>'><?php echo e($character->charName); ?>
-            </a>
+            <a href='character.php?<?php echo http_build_query(['mode' => 'view', 'character' => $character->charName, 'id' => $character->id]); ?>'><?php echo e($character->charName); ?></a>
           </td>
           <td><?php echo e($character->attack); ?></td>
           <td><?php echo e($character->defense); ?></td>
           <td>
             <a href='character.php?<?php echo http_build_query(['mode' => 'edit', 'character' => $character->charName, 'id' => $character->id]) ?>'><img role='link' src='assets/icons/edit_24dp_FILL0_wght400_GRAD0_opsz24.svg' aria-label='edit button' /></a>
-
-            <a role='button' href='index.php?<?php echo http_build_query(['id' => $character->id, 'action' => 'delete']) ?>'><img src='assets/icons/delete_24dp_FILL0_wght400_GRAD0_opsz24.svg' aria-label='delete button' />
+            <a role='button' href='index.php?<?php echo http_build_query(['id' => $character->id, 'action' => 'delete']) ?>'><img src='assets/icons/delete_24dp_FILL0_wght400_GRAD0_opsz24.svg' aria-label='delete button' /></a>
           </td>
         </tr>
       <?php endforeach; ?>
